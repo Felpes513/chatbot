2b1916cc9d3b3f6e8e047af3fab54cf84eade674
Felipe Souza Moreira <felipemoreira2003@gmail.com>
2025-05-05 07:55:16 -0300

    Requirements.txt atualizado

--- app/services/email_solicitacao.py
+++ app/services/email_solicitacao.py
@@ -7,7 +7,7 @@ smtp_server = 'smtp.gmail.com'
 smtp_port = 587
 smtp_user = 'felipemoreira2003@gmail.com'
 smtp_password = 'gnmq rotb stmw tvvx'
-email_suporte = 'felipssouza1974@gmail.com'
+email_suporte = 'felipe.moreira@uscsonline.com.br'
 
 def enviar_email_solicitacao(nome, email_usuario, permissao=None, crm=None, estado_crm=None, caminho_assinatura=None):
     mensagem = MIMEMultipart()
