ca517a7777b5c3dd8ee31a98581e848859fb1dad
Felipe Souza Moreira <felipemoreira2003@gmail.com>
2025-05-07 00:13:01 -0300

    Correção de falhas

--- app/services/buscar_exame.py
+++ app/services/buscar_exame.py
@@ -2,15 +2,31 @@ from app.services.bd import conectar
 
 def buscar_exame_por_nome(nome_paciente):
     conn = conectar()
-    cursor = conn.cursor()
-    query = """
-        SELECT exame_id, nome_paciente, data_exame, unidade, accession_number, status_exame
-        FROM exames
-        WHERE nome_paciente LIKE %s
-    """
+    if not conn:
+        return []
+
     try:
-        cursor.execute(query, (f"%{nome_paciente}%",))
+        cursor = conn.cursor(dictionary=True)
+
+        
+        cursor.execute("""
+            SELECT * FROM exames WHERE nome_paciente = %s
+        """, (nome_paciente,))
         resultados = cursor.fetchall()
+
+        
+        if not resultados:
+            cursor.execute("""
+                SELECT * FROM exames WHERE nome_paciente LIKE %s
+            """, (f"%{nome_paciente}%",))
+            resultados = cursor.fetchall()
+
+        return resultados
+
+    except Exception as e:
+        print(f"Erro ao buscar exame: {e}")
+        return []
+
     finally:
+        cursor.close()
         conn.close()
-    return resultados
